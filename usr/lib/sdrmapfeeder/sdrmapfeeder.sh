592d7a80ca8e973b5160670073bb8203edbbf233
skymaker-c2is <63470599+skymaker-c2is@users.noreply.github.com>
2025-11-05 19:51:09 +0100

    Fix cpuinfo for raspis

--- usr/lib/sdrmapfeeder/sdrmapfeeder.sh
+++ usr/lib/sdrmapfeeder/sdrmapfeeder.sh
@@ -18,7 +18,7 @@ while true; do
 		sysinfolastrun=$(date +"%s")
 	echo "{\
 		\"cpu\":{\
-			\"model\":\"$(cat /proc/cpuinfo |grep 'model name'|tail -n 1|cut -d ':' -f 2)\",\
+			\"model\":\"$(test -f /sys/firmware/devicetree/base/model && tr -d '\0' < /sys/firmware/devicetree/base/model || cat /proc/cpuinfo |grep 'model'|tail -n 1|cut -d ':' -f 2)\",\
 			\"cores\":\"$(cat /proc/cpuinfo |grep -c -e '^processor')\",\
 			\"load\":\"$(cat /proc/loadavg |cut -d ' ' -f 1)\",\
 			\"temp\":\"$(($(cat /sys/class/thermal/thermal_zone*/temp 2>/dev/null |sort -n|tail -n 1)/1000))\",\
