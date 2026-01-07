512244dffb6bf6e393f0b0729672ce6114249898
skymaker-c2is <63470599+skymaker-c2is@users.noreply.github.com>
2026-01-07 19:54:13 +0100

    Fix feeder script

--- usr/lib/sdrmapfeeder/sdrmapfeeder.sh
+++ usr/lib/sdrmapfeeder/sdrmapfeeder.sh
@@ -13,23 +13,23 @@ if [[ -z $username ]] || [[ -z $password ]] || [[ $username == "yourusername" ]]
 fi
 
 while true; do
-	if [ "$sysinfo" = "true" ] && [ $(($(date +"%s") - $sysinfolastrun)) -ge "$sysinfointerval" ]; then
+	if [[ "$sysinfo" == 'true' ]] && [[ $(($(date +"%s") - $sysinfolastrun)) -ge "$sysinfointerval" ]]; then
 		sysinfolastrun=$(date +"%s")
-		
-		if [ "$gps" = "true" ] && command -v gpspipe >/dev/null 2>&1 && command -v jq >/dev/null 2>&1; then
+
+		if [[ "$gps" == 'true' ]] && command -v gpspipe >/dev/null 2>&1 && command -v jq >/dev/null 2>&1; then
 			s=$(gpspipe -w -n 10|grep -m 1 lat)
-			if [ $? -eq 0 ]; then
+			if [[ $? -eq 0 ]]; then
 				gpsLat=$(echo $s|jq '.lat')
 				gpsLon=$(echo $s|jq '.lon')
 			fi
 		fi
-		
+
 		echo "{\
 			\"cpu\":{\
 				\"model\":\"$(test -f /sys/firmware/devicetree/base/model && tr -d '\0' < /sys/firmware/devicetree/base/model || cat /proc/cpuinfo |grep 'model'|tail -n 1|cut -d ':' -f 2)\",\
 				\"cores\":\"$(cat /proc/cpuinfo |grep -c -e '^processor')\",\
 				\"load\":\"$(cat /proc/loadavg |cut -d ' ' -f 1)\",\
-				\"temp\":\"$(($(cat /sys/class/thermal/thermal_zone*/temp 2>/dev/null |sort -n|tail -n 1)/1000))\",\
+				\"temp\":\"$([[ $(ls /sys/class/thermal/thermal_zone* 2>/dev/null | grep -c -w temp) -gt 0 ]] && echo $(($(cat /sys/class/thermal/thermal_zone*/temp |sort -n|tail -n 1 2>/dev/null)/1000)))\",\
 				\"throttled\":\"$(vcgencmd get_throttled 2>/dev/null |cut -d '=' -f 2 )\"\
 			},\
 			\"memory\":{\
@@ -56,12 +56,12 @@ while true; do
 				\"dump1090-mutability\":\"$(dpkg -s dump1090-mutability 2>&1|grep 'Version:'|cut -d ' ' -f 2)\",\
 				\"dump1090-fa\":\"$(dpkg -s dump1090-fa 2>&1|grep 'Version:'|cut -d ' ' -f 2)\",\
 				\"ais-catcher\":\"$(dpkg -s ais-catcher 2>&1 |grep 'Version:'|cut -d ' ' -f 2)\",\
-   				\"radiosondeautorxr\":\"$(dpkg -s radiosondeautorx 2>&1 |grep 'Version:'|cut -d ' ' -f 2)\"\
+   				\"radiosondeautorx\":\"$(dpkg -s radiosondeautorx 2>&1 |grep 'Version:'|cut -d ' ' -f 2)\"\
 			},\
 			\"position\":{\
 				\"enabled\":\"$position\",\
-				\"lat\":\"$($position && $(echo $gpsLat || echo $lat))\",\
-				\"lon\":\"$($position && $(echo $gpsLon || echo $lon))\"
+				\"lat\":\"$([[ $position = 'true' ]] && ([[ ! -z ${gpsLat+x} ]] && echo $gpsLat || echo $lat))\",\
+				\"lon\":\"$([[ $position = 'true' ]] && ([[ ! -z ${gpsLon+x} ]] && echo $gpsLon || echo $lon))\"
 			},\
 			\"feeder\":{\
 				\"version\":\"$version\",\
@@ -70,13 +70,13 @@ while true; do
 		}"| gzip -c |curl -s -u $username:$password -X POST -H "Content-type: application/json" -H "Content-encoding: gzip" --data-binary @- https://sys.feed.sdrmap.org/index.php
 	fi;
 
-	if [ "$adsb" = "true" ]; then
+	if [[ "$adsb" == 'true' ]]; then
 			gzip -c $adsbpath | curl -s -u $username:$password -X POST -H "Content-type: application/json" -H "Content-encoding: gzip" --data-binary @- https://adsb.feed.sdrmap.org/index.php
 	fi
-	
-	if [ "$radiosonde" = "true" ] && [ $(($(date +"%s") - $radiosondelastrun)) -ge "$radiosondeinterval" ]; then
+
+	if [[ "$radiosonde" == 'true' ]] && [[ $(($(date +"%s") - $radiosondelastrun)) -ge "$radiosondeinterval" ]]; then
 		radiosondelastrun=$(date +"%s")
-		if [ ! -d "$radiosondepath" ]; then
+		if [[ ! -d "$radiosondepath" ]]; then
 			echo "The log directory '$radiosondepath' doesn't exist."
 			exit 1
 		fi
